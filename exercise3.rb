d67dff75739903999083916bc0546b9731664d01
vrunda2712 <angy.v27@gmail.com>
2017-05-15 14:52:33 -0400

    exercise3 file been updated with examples

--- exercise3.rb
+++ exercise3.rb
@@ -4,3 +4,18 @@ puts "what is your name?"
 #name ="Vrunda"
 name = gets
 puts "Hi #{name}"
+
+#example of .times method
+3.times do
+  puts "#{name}"
+end
+
+#or you can write it in inline
+  #  3.times {puts "#{name}"}
+
+#example of .each method
+one_to_five = (1..5)
+
+one_to_five.each do |num|
+  puts num**2
+end
