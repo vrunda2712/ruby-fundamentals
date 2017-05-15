800febfd9d3b325480f6f99c3d0bc263728a2150
vrunda2712 <angy.v27@gmail.com>
2017-05-15 13:19:44 -0400

    basic program of ruby

---
+++ exercise1.rb
@@ -0,0 +1,19 @@
+puts 2
+puts 3
+puts 2 + 3
+
+puts "Hello World"
+puts 'hello world'
+
+puts "Betty's Shop"
+puts 'betty\'s shop'
+
+puts "Ada Lovelace lived for #{1852-1815} years."
+
+puts "Hello\t\tworld"
+
+puts "Hello\b\b\b\sGoodbye world"
+
+puts "Hello Start\r over world"
+
+puts "1. Hello\n2. World"
